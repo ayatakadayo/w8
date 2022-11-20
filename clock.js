7a3539463f6cc2b1719d1ff4cd64af7dc467b922
ayatakadayo <113568955+ayatakadayo@users.noreply.github.com>
2022-11-20 12:30:46 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -5,6 +5,8 @@ let clock = null;
 const digitalClock = ()=>{
     // 現在時刻取得
     const now = new Date();
+    
+    const youbi = ["日","月","火","水","木","金","土"];
 
     // 時・分・秒取得
     const [hour, minute, second] = [now.getHours(), now.getMinutes(), now.getSeconds()];
@@ -17,6 +19,12 @@ const digitalClock = ()=>{
         String(minute).padStart(2, "0"),
         String(second).padStart(2, "0"),
     ];
+    
+    const date = now.getFullYear() + "年" + 
+				(now.getMonth() + 1)  + "月" + 
+				now.getDate() + "日" + 
+				youbi[now.getDay()] + "曜日"
+	console.log(date);
 
     // 表示
     const txtDigitalClock = `${txtHour}:${txtMinute}:${txtSecond}`;
