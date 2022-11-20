6a78b71af62d59b290321ca671649811b8ef2bc3
ayatakadayo <113568955+ayatakadayo@users.noreply.github.com>
2022-11-20 11:53:55 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,37 +1,32 @@
-(()=>{
-            // ゼロ埋めして2桁の数値にする
-            const zero = n => (n < 10 ) ? "0" + n.toString() : n.toString();
- 
-            // 日付の文字列化
-            const youbi = ["日","月","火","水","木","金","土"];
-            const getDateString = date =>
-                `${ date.getFullYear() }年 ${ zero(date.getMonth() + 1) }月  ${ zero(date.getDate()) }日 ${ youbi[date.getDay()] }曜日`;
- 
-            // 時間の文字列化
-            const getHourString = date =>
-                `${ zero(date.getHours()) }: ${ zero(date.getMinutes()) }: ${ zero(date.getSeconds()) }`;
- 
-            // DOMの構築を待つ
-            window.addEventListener('DOMContentLoaded',()=> {
-                // 日時を表示するDOM要素を取得
-                const dateDiv = document.getElementById("date");
-                const clockDiv = document.getElementById("clock");
- 
-                // 現在の日
-                let nowDate = null;
- 
-                // 1秒周期のタイマーセット
-                setInterval( ()=>{
-                    // 現在の日時を取得
-                    const now = new Date();
-                    // 日付が変わったら日付を再表示
-                    if( nowDate !== now.getDate() ) {
-                        nowDate = now.getDate();
-                        dateDiv.innerText = getDateString(now);
-                    }
- 
-                    // 時間を再表示
-                    clockDiv.innerText = getHourString(now);
-                },1000);
-            });
-})();
+// デジタル時計表示用
+let clock = null;
+
+// デジタル時計を表示する
+const digitalClock = ()=>{
+    // 現在時刻取得
+    const now = new Date();
+
+    // 時・分・秒取得
+    const [hour, minute, second] = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+    // 0埋め処理
+    const [txtHour, txtMinute, txtSecond] = [
+        String(hour).padStart(2, "0"),
+        String(minute).padStart(2, "0"),
+        String(second).padStart(2, "0"),
+    ];
+
+    // 表示
+    const txtDigitalClock = `${txtHour}:${txtMinute}:${txtSecond}`;
+    clock.textContent = txtDigitalClock;
+    requestAnimationFrame(digitalClock);
+};
+
+// 起動時の処理
+window.addEventListener("load", ()=>{
+    // 時計表示部分DOM取得
+    clock = document.getElementById("clock");
+
+    // デジタル時計開始
+    digitalClock();
+});
