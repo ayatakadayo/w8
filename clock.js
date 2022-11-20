e0a38323b22aeb624defe5acf57f1a6e8a1d9638
ayatakadayo <113568955+ayatakadayo@users.noreply.github.com>
2022-11-20 15:20:25 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,4 +1,11 @@
 let clock = null;
+
+var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth();
+    var day = today.getDate();
+    console.log(year + '年' + month + '月' + day + '日');
+
 const digitalClock = ()=>{
     const now = new Date();
 
